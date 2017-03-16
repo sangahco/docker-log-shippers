733e3db272b1deb995aacb490aecacef08275ccd
Emanuele Disco <emawind84@gmail.com>
2017-03-16 15:12:13 +0900

    bug fix

--- docker17-auto.sh
+++ docker17-auto.sh
@@ -3,8 +3,7 @@
 set -e
 
 REGISTRY_URL="${REGISTRY_URL:-$(cat .env | awk 'BEGIN { FS="="; } /^REGISTRY_URL/ {sub(/\r/,"",$2); print $2;}')}"
-LOGSTASH_HOST="${LOGSTASH_HOST:-$(cat .env | awk 'BEGIN { FS="="; } /^LOGSTASH_HOST/ {sub(/\r/,"",$2); print $2;}')}"
-
+LOGSTASH_URL="${LOGSTASH_URL:-$(cat .env | awk 'BEGIN { FS="="; } /^LOGSTASH_URL/ {sub(/\r/,"",$2); print $2;}')}"
 LOG_PATH="${LOG_PATH:-$(cat .env | awk 'BEGIN { FS="="; } /^LOG_PATH/ {sub(/\r/,"",$2); print $2;}')}"
 LOG2_PATH="${LOG2_PATH:-$(cat .env | awk 'BEGIN { FS="="; } /^LOG2_PATH/ {sub(/\r/,"",$2); print $2;}')}"
 LOG3_PATH="${LOG3_PATH:-$(cat .env | awk 'BEGIN { FS="="; } /^LOG3_PATH/ {sub(/\r/,"",$2); print $2;}')}"
